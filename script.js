7bfe4bc3e4e57fa0851fec10bbc9fb55ffefbdd6
Joshua Tzucker <joshua.tz@gmail.com>
2020-06-15 11:37:56 -0700

    WIP: Advanced options, formatting
     - Using Prettier for formatting
     - Added advanced options
     - Working on UI and preview mode

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const CACHE_KEY = 'cache';
+
+/**
+ * Elements
+ */
 /** @type {HTMLTextAreaElement} */
 const outputArea = document.getElementById('outputTextArea');
 /** @type {HTMLInputElement} */
@@ -5,15 +10,44 @@ const urlInputElem = document.getElementById('tweetUrlInput');
 const generateButton = document.getElementById('generateButton');
 const placeholderOutTxt = outputArea.value;
 const previewArea = document.getElementById('previewArea');
+const inputForm = document.getElementById('inputForm');
+const defaultHeightInput = document.getElementById('defaultHeight');
+const removeBorderCheckbox = document.getElementById('removeBorder');
+const sandboxCheckbox = document.getElementById('sandbox');
 
+/**
+ * Cache
+ */
+const getCache = () => {
+	const cacheStr = localStorage.getItem(CACHE_KEY) || `{}`;
+	return JSON.parse(cacheStr);
+};
+const updateCache = (cache) => {
+	localStorage.setItem(CACHE_KEY, JSON.stringify(cache));
+};
+let cache = getCache();
+
+// Main Generate Func
 const generate = async () => {
+	const config = getConfig();
+	console.log(config);
 	if (urlInputElem.value && urlInputElem.checkValidity()) {
 		const tweetUrl = urlInputElem.value;
 		try {
 			const embedInfo = await getOEmbed(tweetUrl);
-			// dataUriIframeInject(embedInfo.html, previewArea, true);
-			const dataUri = `data:text/html;charset=utf-8,${escape(embedInfo.html)}`;
-			const iframeCodeStr = getIframeStr(dataUri, embedInfo.width, embedInfo.height);
+			let { html, width, height } = embedInfo;
+			height = height || config.defaultHeight;
+			let iframeCodeStr = '';
+			let iframeStyleStr = '';
+			if (config.removeBorder) {
+				iframeStyleStr = 'border:none;';
+			}
+			if (config.iframeType === 'dataUri') {
+				const dataUri = `data:text/html;charset=utf-8,${escape(html)}`;
+				iframeCodeStr = getIframeStrWithUri(dataUri, iframeStyleStr, width, height, config.sandbox);
+			} else {
+				iframeCodeStr = getIframeStrWithSrcDoc(simpleEscape(html), iframeStyleStr, width, height, config.sandbox);
+			}
 			outputArea.value = iframeCodeStr;
 			previewArea.innerHTML = iframeCodeStr;
 		} catch (e) {
@@ -25,8 +59,12 @@ const generate = async () => {
 	} else {
 		alert('Please enter a valid URL 🙃');
 	}
-}
+};
 
+/**
+ * Reset form
+ * @param {boolean} resetUrl should URL input be reset?
+ */
 const reset = (resetUrl) => {
 	outputArea.disabled = true;
 	outputArea.value = placeholderOutTxt;
@@ -34,32 +72,59 @@ const reset = (resetUrl) => {
 	if (resetUrl) {
 		urlInputElem.value = '';
 	}
-}
+};
 
-const dataUriIframeInject = (t,e,a,n) => {n="function"==typeof n?n:function(){},a="boolean"!=typeof a||a;var i=document.createElement("iframe"),c="data:text/html;charset=utf-8,"+escape(t);i.addEventListener("load",n),i.setAttribute("src",c),a?(e.appendChild(i),i.style.width="100%",i.style.height="100%"):e.replaceWith(i)}
+const getConfig = () => {
+	/** @type {Config} */
+	const config = {
+		iframeType: document.querySelector('input[name="iframeType"]:checked').value,
+		defaultHeight: parseInt(defaultHeightInput.value, 10),
+		removeBorder: !!removeBorderCheckbox.checked,
+		sandbox: !!sandboxCheckbox.checked
+	};
+	return config;
+};
 
-const getIframeStr = (dataUri, optWidth, optHeight) => {
+const dataUriIframeInject = (t, e, a, n) => {
+	(n = 'function' == typeof n ? n : function () {}), (a = 'boolean' != typeof a || a);
+	var i = document.createElement('iframe'),
+		c = 'data:text/html;charset=utf-8,' + escape(t);
+	i.addEventListener('load', n), i.setAttribute('src', c), a ? (e.appendChild(i), (i.style.width = '100%'), (i.style.height = '100%')) : e.replaceWith(i);
+};
+
+const getIframeStrWithUri = (dataUri, optStyle = '', optWidth, optHeight, optSandbox = false) => {
 	return `
-<iframe src="${dataUri}" ${optWidth ? `width="${optWidth}" ` : ''}${optHeight ? `width="${optHeight}" ` : ''}></iframe>;
-`
-}
+<iframe src="${dataUri}" style="${optStyle}" ${optWidth ? `width="${optWidth}" ` : ''}${optHeight ? `height="${optHeight}" ` : ''}${optSandbox ? `sandbox ` : ``}></iframe>
+`;
+};
+
+const getIframeStrWithSrcDoc = (rawHtml, optStyle = '', optWidth, optHeight, optSandbox = false) => {
+	return `
+<iframe srcdoc="${rawHtml}" style="${optStyle}" ${optWidth ? `width="${optWidth}" ` : ''}${optHeight ? `height="${optHeight}" ` : ''}${optSandbox ? `sandbox ` : ``}></iframe>
+`;
+};
 
 /**
  * API Stuff
  */
 /**
- * 
- * @param {string} tweetIdOrUrl 
- * @param {Partial<GetOEmbedParams>} optParams 
+ *
+ * @param {string} tweetIdOrUrl
+ * @param {Partial<GetOEmbedParams>} optParams
+ * @param {boolean} optUseCache
  * @returns {OEmbedRes}
  */
-const getOEmbed = async (tweetIdOrUrl, optParams = {}) => {
+const getOEmbed = async (tweetIdOrUrl, optParams = {}, optUseCache = true) => {
+	if (optUseCache && !!cache[tweetIdOrUrl]) {
+		console.log('Used cache :)');
+		return cache[tweetIdOrUrl];
+	}
 	const endpoint = 'https://publish.twitter.com/oembed';
 	/** @type {GetOEmbedParams} */
 	const params = {
 		...optParams,
 		url: tweetIdOrUrl
-	}
+	};
 	const url = new URL(endpoint);
 	url.search = new URLSearchParams(params);
 
@@ -68,6 +133,10 @@ const getOEmbed = async (tweetIdOrUrl, optParams = {}) => {
 		let resolved = false;
 		fetchViaJsonp(url.toString(), (data) => {
 			resolved = true;
+			if ('html' in data) {
+				cache[tweetIdOrUrl] = data;
+				updateCache(cache);
+			}
 			res(data);
 		});
 		setTimeout(() => {
@@ -76,11 +145,11 @@ const getOEmbed = async (tweetIdOrUrl, optParams = {}) => {
 			}
 		}, 5000);
 	});
-}
+};
 
 const fetchViaJsonp = (url, callback) => {
 	var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
-	window[callbackName] = function(data) {
+	window[callbackName] = function (data) {
 		delete window[callbackName];
 		document.body.removeChild(script);
 		callback(data);
@@ -89,14 +158,38 @@ const fetchViaJsonp = (url, callback) => {
 	var script = document.createElement('script');
 	script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
 	document.body.appendChild(script);
-}
+};
+
+/**
+ * Utilities
+ */
+const simpleEscape = (input) => {
+	return input.replace(/&/g, '&amp;').replace(/"/g, `'`);
+};
 
 /**
  * Setup Event Listeners
  */
-generateButton.addEventListener('click', generate);
+let generationInProgress = false;
+generateButton.addEventListener('click', () => {
+	if (!generationInProgress) {
+		generationInProgress = true;
+		generate().then(() => {
+			generationInProgress = false;
+		});
+	}
+});
 urlInputElem.addEventListener('change', () => {
 	reset(false);
+});
+inputForm.addEventListener('submit', (evt) => {
+	evt.preventDefault();
+	if (!generationInProgress) {
+		generationInProgress = true;
+		generate().then(() => {
+			generationInProgress = false;
+		});
+	}
 });
 
 /**
@@ -130,4 +223,12 @@ urlInputElem.addEventListener('change', () => {
  * @property {string} provider_name
  * @property {string} provider_url
  * @property {string} version
+ */
+
+/**
+ * @typedef Config
+ * @property {'srcDoc' | 'dataUri'} iframeType
+ * @property {number} defaultHeight
+ * @property {boolean} removeBorder
+ * @property {boolean} sandbox
  */
